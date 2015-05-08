30906ee1f5bc6f6713d811c0e08146c24e5aa4e6
edwin simpson <edwin@robots.ox.ac.uk>
2015-05-08 03:30:33 +0100

    revised prediction tests

--- python/prediction_tests.py
+++ python/prediction_tests.py
@@ -12,173 +12,7 @@ from ibcc import IBCC
 from gpgrid import GPGrid
 from scipy.sparse import coo_matrix
 from scipy.stats import gaussian_kde
-import matplotlib.pyplot as plt
-
-class UshahidiDataHandler(object):
-    
-    K = 1
-    nx = 1000
-    ny = 1000
-    
-    minlat = 18.2#18.0
-    maxlat = 18.8#19.4
-    minlon = -72.6#-73.1
-    maxlon = -72.0#-71.7   
-    
-    datadir = './data'
-    
-    C = []
-    
-    #Dictionary defining how we map original categories 
-    #to class IDs, using original categories as keys:
-    categorymap = {1:1, 3:1,5:{'a':1},6:{'a':1,'b':1}} 
-    
-    def __init__(self, nx, ny, datadir):
-        self.nx = nx
-        self.ny = ny
-        self.datadir = datadir
-        
-    def translate_points_to_local(self, latdata, londata):
-        logging.debug('Translating original coords to local values.')
-            
-        latdata = np.float64(latdata)
-        londata = np.float64(londata)
-        
-        normlatdata = (latdata-self.minlat)/(self.maxlat-self.minlat)
-        normlondata = (londata-self.minlon)/(self.maxlon-self.minlon)    
-            
-        latdata = np.array(np.round(normlatdata*self.nx), dtype=np.int)
-        londata = np.array(np.round(normlondata*self.ny), dtype=np.int)
-            
-        return latdata,londata           
-    
-    def load_ush_data(self):
-        dataFile = self.datadir+'/exported_ushahidi.csv'
-        self.K = 1
-        #load the data
-    #     reportIDdata = np.genfromtxt(dataFile, np.str, delimieter=',', skip_header=True, usecols=[])
-    #     datetimedata = np.genfromtxt(dataFile, np.datetime64, delimiter=',', skip_header=True, usecols=[2,3])
-        latdata = np.genfromtxt(dataFile, np.float64, delimiter=',', skip_header=True, usecols=[4])
-        londata = np.genfromtxt(dataFile, np.float64, delimiter=',', skip_header=True, usecols=[5])
-        reptypedata = np.genfromtxt(dataFile, np.str, delimiter=',', skip_header=True, usecols=[1])
-        rep_list_all = np.genfromtxt(dataFile, np.str, delimiter=',', skip_header=True, usecols=[6])
-        latdata,londata = self.translate_points_to_local(latdata,londata)
-        rep_id_grid = {}
-
-        rep_list = {}
-        C = {}            
-        instantiatedagents = []
-
-        for i, reptypetext in enumerate(reptypedata):        
-            typetoks = reptypetext.split('.')
-            typetoks = typetoks[0].split(',')
-            maintype = 1
-            if "1a." in reptypetext: # Trapped people
-                agentID = 0
-            elif "1b." in reptypetext: 
-                agentID = 1
-            elif "1c." in reptypetext:
-                agentID = 2
-            elif "1" in reptypetext or "1d." in reptypetext:
-                agentID = 3
-            elif "2a." in reptypetext:
-                agentID = 4
-            elif "2b." in reptypetext:
-                agentID = 5
-            elif "2c." in reptypetext:
-                agentID = 6
-            elif "2d." in reptypetext:
-                agentID = 7
-            elif "2e." in reptypetext:
-                agentID = 8
-            elif "2f." in reptypetext:
-                agentID = 9
-            elif "2g." in reptypetext:
-                agentID = 10              
-            elif "3a." in reptypetext:
-                agentID = 11
-            elif "3." in reptypetext or "3b." in reptypetext or "3c." in reptypetext or "3d." in reptypetext or \
-                "3e." in reptypetext  or "6b." in reptypetext:
-                agentID = 12
-            elif "4a." in reptypetext:
-                agentID = 13
-            elif "4." in reptypetext or "4b." in reptypetext:
-                agentID = 14
-            elif "5a." in reptypetext:
-                agentID = 15
-            elif "5." in reptypetext or "5c." in reptypetext or "5d." in reptypetext:
-                agentID = 16
-            elif "6." in reptypetext or "6a." in reptypetext:
-                agentID = 17
-            elif "7a." in reptypetext:
-                agentID = 18     
-            elif "7b." in reptypetext:
-                agentID = 19
-            elif "7c." in reptypetext:
-                agentID = 20
-            elif "7." in reptypetext or "7d." in reptypetext or "7e." in reptypetext or "7f." in reptypetext:
-                agentID = 21
-            elif "8a." in reptypetext:
-                agentID = 22
-            elif "8b." in reptypetext or "6c." in reptypetext or "6d." in reptypetext:
-                agentID = 23
-            elif "8c." in reptypetext:
-                agentID = 24
-            elif "8e." in reptypetext or "8." in reptypetext or  "8d." in reptypetext or "8f." in reptypetext:
-                agentID = 25                
-            else: #we don't care about these categories in the demo anyway, but can add them easily here
-                agentID = 26
-                for typestring in typetoks:
-                    if len(typestring)>1:
-                        sectype = typestring[1] # second character should be a letter is available
-                    else:
-                        sectype = 0
-                    try:
-                        maintype = int(typestring[0]) #first character should be a number
-                    except ValueError:
-                        logging.warning('Not a report category: ' + typestring)
-                        continue                   
-                    if maintype in self.categorymap:
-                        mappedID = self.categorymap[maintype]
-                        if type(mappedID) is dict:
-                            if sectype in mappedID:
-                                maintype = mappedID[sectype]
-                        else:
-                            maintype = mappedID
-
-            repx = latdata[i]
-            repy = londata[i]
-            if repx>=self.nx or repx<0 or repy>=self.ny or repy<0:
-                continue
-            
-            try:
-                Crow = np.array([agentID, repx, repy, 1]) # all report values are 1 since we only have confirmations of an incident, not confirmations of nothing happening
-            except ValueError:
-                logging.error('ValueError creating a row of the crowdsourced data matrix.!')        
-
-            if C=={} or maintype not in C:
-                C[maintype] = Crow.reshape((1,4))
-                rep_id_grid[maintype] = np.empty((self.nx, self.ny), dtype=np.object)
-                rep_list[maintype] = [rep_list_all[i]]
-            else:
-                C[maintype] = np.concatenate((C[maintype], Crow.reshape(1,4)), axis=0)
-                rep_list[maintype].append(rep_list_all[i])
-                
-            if rep_id_grid[maintype][repx, repy] == None:
-                rep_id_grid[maintype][repx, repy] = []
-            rep_id_grid[maintype][repx, repy].append(len(rep_list[maintype])-1)               
-            
-            if agentID not in instantiatedagents:
-                instantiatedagents.append(agentID)
-                
-        instantiatedagents = np.array(instantiatedagents)
-        for j in C.keys():
-            for r in range(C[j].shape[0]):
-                C[j][r,0] = np.argwhere(instantiatedagents==C[j][r,0])[0,0]
-        self.K = len(instantiatedagents)
-                     
-        self.C = C
-        print "Number of type one reports: " + str(self.C[1].shape[0])
+from ushahididata import UshahidiDataHandler
 
 if __name__ == '__main__':
     print "Run tests to determine the accuracy of the bccheatmaps method."
@@ -192,6 +26,9 @@ if __name__ == '__main__':
 #     tgrid = np.genfromtxt(goldfile).astype(int)
     goldfile = "./data/haiti_unosat_target3.npy"
     targets = np.load(goldfile).astype(int)
+    
+    # SUBSET FOR TESTING
+#     targets = targets[0:1000,:]
     
     targetsx = targets[:,0]
     targetsy = targets[:,1]
@@ -215,16 +52,12 @@ if __name__ == '__main__':
     alpha0[:,:,range(15,18)] = np.array([[2.0,1.0],[1.0,2.0]])[:,:,np.newaxis]
     alpha0_all = alpha0
     # set an uninformative prior over the spatial GP
-    nu0 = np.array([1.1, 1.0])    
+    nu0 = np.array([1.0, 1.0])    
     
 #     #subset for testing
 #     C = C[1:100,:]
 #     alpha0 = alpha0[:, :, np.sort(np.unique(C[:,0]))]
 #     K = len(np.unique(C[:,0]))
-    
-    plt.figure()
-    plt.scatter(targetsx, targetsy)
-    plt.show()
     
     print "No. report types = %i" % K
     
@@ -232,8 +65,12 @@ if __name__ == '__main__':
     auc_all = {}
     mce_all = {}
     acc_all = {}
+    # number of available data points
     navailable = C.shape[0]
-    nlabels = 100
+    # number of labels in first iteration dataset
+    nlabels = 250
+    # increment the number of labels at each iteration
+    stepsize = 250
     while nlabels <= navailable:
         C = C_all[0:nlabels, :]
         agents = np.unique(C[:,0])
@@ -241,7 +78,8 @@ if __name__ == '__main__':
         alpha0 = alpha0_all[:,:,agents]
         
         # containers for results
-        results = {}
+        if not 'results' in globals():
+            results = {}
                 
         # indicator array to show whether reports are positive or negative
         posreports = (C[:,3]==1).astype(float)
@@ -261,10 +99,10 @@ if __name__ == '__main__':
         # Coordinates where we made observations, i.e. without duplicates due to multiple reports at some points 
         obs_coords = np.argwhere(obs_grid.toarray()>0)
         obsx = obs_coords[:, 0]
-        obsy = obs_coords[:, 1]          
-                
+        obsy = obs_coords[:, 1]                  
+                 
         # KERNEL DENSITY ESTIMATION ---------------------------------------------------------------------------------------
-        
+         
         # reuse values for training points given by frequencies
         #kernels and sum. Iterate and try to minimise L2 risk to select kernel bandwidth (mean integrated squared error). 
         # This is given by: expected difference between kernel function evaluated at a point and the true value. Can look at
@@ -309,41 +147,41 @@ if __name__ == '__main__':
         #run GP
         gpgrid = GPGrid(nx, ny)
         gpgrid.fit([obsx, obsy], density_estimates)
-        gp_preds, _ = gpgrid.predict([targetsx, targetsy])
+        gp_preds = gpgrid.predict([targetsx, targetsy])
         results['Train_GP_on_Freq'] = gp_preds        
         
         # RUN SEPARATE IBCC AND GP STAGES ----------------------------------------------------------------------------------
-        
+         
         # run standard IBCC
         combiner = IBCC(2, 2, alpha0, nu0, K)
         combiner.min_iterations = 5
         combiner.max_iterations = 200
         combiner.conv_threshold = 0.1
-        
+         
         #flatten the input data so it can be used with standard IBCC
         linearIdxs = np.ravel_multi_index((reportsx, reportsy), dims=(nx,ny))
         C_flat = C[:,[0,1,3]]
         C_flat[:,1] = linearIdxs
         bcc_pred = combiner.combine_classifications(C_flat)
         bcc_pred = bcc_pred[np.ravel_multi_index((obsx, obsy), dims=(nx,ny)), 1]    
-    
+     
         # use IBCC output to train GP
         gpgrid = GPGrid(nx, ny)
         gpgrid.fit([obsx, obsy], bcc_pred)
         gp_preds, _ = gpgrid.predict([targetsx, targetsy])
-        
+         
         results['IBCC_then_GP'] = gp_preds        
-            
+             
         # RUN HEATMAP BCC --------------------------------------------------------------------------------------------------
         combiner = HeatMapBCC(nx, ny, 2, 2, alpha0, nu0, K, force_update_all_points=True, outputx=targetsx, outputy=targetsy)
         combiner.min_iterations = 5
         combiner.max_iterations = 200
         combiner.conv_threshold = 0.1
-    
+     
         # Need to replace with optimised version!
         bcc_pred = combiner.combine_classifications(C)
         bcc_pred = bcc_pred[1,:] # only interested in positive "damage class"
-        
+         
         results['heatmapbcc'] = bcc_pred
                 
         # EVALUATE ALL RESULTS ---------------------------------------------------------------------------------------------
@@ -375,7 +213,7 @@ if __name__ == '__main__':
         elif C_all.shape[0]-nlabels<100:
             nlabels = C_all.shape[0] 
         else:
-            nlabels += 400
+            nlabels += stepsize
             
     np.save("./data/output/results.npy", results_all)
     np.save("./data/output/acc.npy", acc_all)
---
+++ python/ushahididata.py
@@ -0,0 +1,174 @@
+'''
+Created on 7 May 2015
+
+@author: edwin
+'''
+
+import numpy as np
+import logging
+
+class UshahidiDataHandler(object):
+    
+    K = 1
+    nx = 1000
+    ny = 1000
+    
+    minlat = 18.2#18.0
+    maxlat = 18.8#19.4
+    minlon = -72.6#-73.1
+    maxlon = -72.0#-71.7   
+    
+    datadir = './data'
+    
+    C = []
+    
+    #Dictionary defining how we map original categories 
+    #to class IDs, using original categories as keys:
+    categorymap = {1:1, 3:1,5:{'a':1},6:{'a':1,'b':1}} 
+    
+    def __init__(self, nx, ny, datadir):
+        self.nx = nx
+        self.ny = ny
+        self.datadir = datadir
+        
+    def translate_points_to_local(self, latdata, londata):
+        logging.debug('Translating original coords to local values.')
+            
+        latdata = np.float64(latdata)
+        londata = np.float64(londata)
+        
+        normlatdata = (latdata-self.minlat)/(self.maxlat-self.minlat)
+        normlondata = (londata-self.minlon)/(self.maxlon-self.minlon)    
+            
+        latdata = np.array(np.round(normlatdata*self.nx), dtype=np.int)
+        londata = np.array(np.round(normlondata*self.ny), dtype=np.int)
+            
+        return latdata,londata           
+    
+    def load_ush_data(self):
+        dataFile = self.datadir+'/exported_ushahidi.csv'
+        self.K = 1
+        #load the data
+    #     reportIDdata = np.genfromtxt(dataFile, np.str, delimieter=',', skip_header=True, usecols=[])
+    #     datetimedata = np.genfromtxt(dataFile, np.datetime64, delimiter=',', skip_header=True, usecols=[2,3])
+        latdata = np.genfromtxt(dataFile, np.float64, delimiter=',', skip_header=True, usecols=[4])
+        londata = np.genfromtxt(dataFile, np.float64, delimiter=',', skip_header=True, usecols=[5])
+        reptypedata = np.genfromtxt(dataFile, np.str, delimiter=',', skip_header=True, usecols=[1])
+        rep_list_all = np.genfromtxt(dataFile, np.str, delimiter=',', skip_header=True, usecols=[6])
+        latdata,londata = self.translate_points_to_local(latdata,londata)
+        rep_id_grid = {}
+
+        rep_list = {}
+        C = {}            
+        instantiatedagents = []
+
+        for i, reptypetext in enumerate(reptypedata):        
+            typetoks = reptypetext.split('.')
+            typetoks = typetoks[0].split(',')
+            maintype = 1
+            if "1a." in reptypetext: # Trapped people
+                agentID = 0
+            elif "1b." in reptypetext: 
+                agentID = 1
+            elif "1c." in reptypetext:
+                agentID = 2
+            elif "1" in reptypetext or "1d." in reptypetext:
+                agentID = 3
+            elif "2a." in reptypetext:
+                agentID = 4
+            elif "2b." in reptypetext:
+                agentID = 5
+            elif "2c." in reptypetext:
+                agentID = 6
+            elif "2d." in reptypetext:
+                agentID = 7
+            elif "2e." in reptypetext:
+                agentID = 8
+            elif "2f." in reptypetext:
+                agentID = 9
+            elif "2g." in reptypetext:
+                agentID = 10              
+            elif "3a." in reptypetext:
+                agentID = 11
+            elif "3." in reptypetext or "3b." in reptypetext or "3c." in reptypetext or "3d." in reptypetext or \
+                "3e." in reptypetext  or "6b." in reptypetext:
+                agentID = 12
+            elif "4a." in reptypetext:
+                agentID = 13
+            elif "4." in reptypetext or "4b." in reptypetext:
+                agentID = 14
+            elif "5a." in reptypetext:
+                agentID = 15
+            elif "5." in reptypetext or "5c." in reptypetext or "5d." in reptypetext:
+                agentID = 16
+            elif "6." in reptypetext or "6a." in reptypetext:
+                agentID = 17
+            elif "7a." in reptypetext:
+                agentID = 18     
+            elif "7b." in reptypetext:
+                agentID = 19
+            elif "7c." in reptypetext:
+                agentID = 20
+            elif "7." in reptypetext or "7d." in reptypetext or "7e." in reptypetext or "7f." in reptypetext:
+                agentID = 21
+            elif "8a." in reptypetext:
+                agentID = 22
+            elif "8b." in reptypetext or "6c." in reptypetext or "6d." in reptypetext:
+                agentID = 23
+            elif "8c." in reptypetext:
+                agentID = 24
+            elif "8e." in reptypetext or "8." in reptypetext or  "8d." in reptypetext or "8f." in reptypetext:
+                agentID = 25                
+            else: #we don't care about these categories in the demo anyway, but can add them easily here
+                agentID = 26
+                for typestring in typetoks:
+                    if len(typestring)>1:
+                        sectype = typestring[1] # second character should be a letter is available
+                    else:
+                        sectype = 0
+                    try:
+                        maintype = int(typestring[0]) #first character should be a number
+                    except ValueError:
+                        logging.warning('Not a report category: ' + typestring)
+                        continue                   
+                    if maintype in self.categorymap:
+                        mappedID = self.categorymap[maintype]
+                        if type(mappedID) is dict:
+                            if sectype in mappedID:
+                                maintype = mappedID[sectype]
+                        else:
+                            maintype = mappedID
+
+            repx = latdata[i]
+            repy = londata[i]
+            if repx>=self.nx or repx<0 or repy>=self.ny or repy<0:
+                continue
+            
+            try:
+                Crow = np.array([agentID, repx, repy, 1]) # all report values are 1 since we only have confirmations of an incident, not confirmations of nothing happening
+            except ValueError:
+                logging.error('ValueError creating a row of the crowdsourced data matrix.!')        
+
+            if C=={} or maintype not in C:
+                C[maintype] = Crow.reshape((1,4))
+                rep_id_grid[maintype] = np.empty((self.nx, self.ny), dtype=np.object)
+                rep_list[maintype] = [rep_list_all[i]]
+            else:
+                C[maintype] = np.concatenate((C[maintype], Crow.reshape(1,4)), axis=0)
+                rep_list[maintype].append(rep_list_all[i])
+                
+            if rep_id_grid[maintype][repx, repy] == None:
+                rep_id_grid[maintype][repx, repy] = []
+            rep_id_grid[maintype][repx, repy].append(len(rep_list[maintype])-1)               
+            
+            if agentID not in instantiatedagents:
+                instantiatedagents.append(agentID)
+                
+        instantiatedagents = np.array(instantiatedagents)
+        for j in C.keys():
+            for r in range(C[j].shape[0]):
+                C[j][r,0] = np.argwhere(instantiatedagents==C[j][r,0])[0,0]
+        self.K = len(instantiatedagents)
+                     
+        self.C = C
+        print "Number of type one reports: " + str(self.C[1].shape[0])
